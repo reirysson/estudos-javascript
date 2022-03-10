61e1ee4e37618e0cbb3cb32ca9734d6daeb9b1db
reirysson <reiryssonjp@gmail.com>
2022-03-09 22:52:47 -0300

    terceira lista de exercicios

---
+++ conversoes.js
@@ -0,0 +1,10 @@
+let minhaLet;
+console.log(minhaLet); //undefined
+
+// atribuindo um valor com TIPO DE DADO STRING
+minhaLet = "eu sou um texto";
+console.log(minhaLet); // "eu sou um texto"
+
+// reatribuindo, desta vez com dado do TIPO NUMBER
+minhaLet = 100;
+console.log(minhaLet); //100
