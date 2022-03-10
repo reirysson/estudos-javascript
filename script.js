19e6cb8ae81b8890065aa5b65d293e9dc34759db
reirysson <reiryssonjp@gmail.com>
2022-03-09 22:27:38 -0300

    segunda linha de exercícios

---
+++ script.js
@@ -0,0 +1,25 @@
+//let forma = 'retângulo';
+//let altura = 5;
+//let comprimento = 7;
+//let area;
+
+//if (forma === 'retângulo'){
+//    area = altura * comprimento;
+//} else {
+//    area = (altura * comprimento) / 2;
+//}
+
+//console.log(area);
+
+const forma = 'triângulo';
+const altura = 5;
+const comprimetno = 7;
+let area;
+
+if (forma === 'quadrado') {
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) /2;
+}
+
+console.log(area);
